5592c56671b319cff9b358e972ec6437f907de11
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-07-13 17:33:36 -0400

    basic structure is set up.  Dark mode is functioning when clicked. Search results containers hard coded for now

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hi Liz");
+
+document
+  .getElementById("toggle-theme")
+  .addEventListener("click", function (event) {
+    console.log("Theme Changed");
+    document.body.classList.toggle("dark-mode");
+  });
