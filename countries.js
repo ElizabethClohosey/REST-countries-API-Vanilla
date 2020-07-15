0a2c379e7263394348ff9ec7ed44a9b7e0596160
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-07-15 15:18:11 -0400

    each country info result is working when clicked and opening to detail page

--- countries.js
+++ countries.js
@@ -2,22 +2,23 @@
 let resultsSection = document.querySelector(".search-results");
 
 // Adding container where results will populate to DOM
-let resultsContainer = document.createElement("div");
-resultsContainer.setAttribute("class", "results-container");
-resultsSection.appendChild(resultsContainer);
+let resultsContainer = creteResultsContainer();
 
 // Adding row to results container for cols to populate
-let resultsRow = document.createElement("div");
-resultsRow.setAttribute("class", "row mt-5 results-row");
-resultsContainer.appendChild(resultsRow);
+let resultsRow = createResultsRow();
 
 // Global Variables
 let countries;
 let regionList = document.querySelector("#region");
 let countriesList = document.querySelector("#searched-country");
+// let details;
+let countryDetail = false;
+console.log("Should be false", countryDetail);
 
-// Searching by region
+// Searching by region on change event for select dropdown
 regionList.addEventListener("change", (event) => {
+  countryDetail = true;
+  console.log("Should be true", countryDetail);
   // Getting value from the region select menu
   let chosenRegion = event.target.value;
 
@@ -27,29 +28,14 @@ regionList.addEventListener("change", (event) => {
   )
     .then((res) => res.json())
     // .then((data) => initialize(data))
-    .then((data) => displayRegionInfo(data)) // Displaying search results
+    .then((data) => displayCountryInfo(data)) // Displaying search results
     .catch((err) => console.log("ERROR", err));
-
-  // Function to display region search results
-  const displayRegionInfo = (countryData) => {
-    countries = countryData;
-
-    countries.forEach((country) => {
-      // document.querySelector(".search-results").empty;
-      // Adding to search results section
-      let resultsCols = createCols();
-      let countryInfo = createInfoDiv(resultsCols);
-      getFlagImg(country, countryInfo);
-      let countryInfoBody = createInfoBody(countryInfo);
-      getName(countryInfoBody, country);
-      getPopulation(countryInfoBody, country);
-      getRegion(countryInfoBody, country);
-      getCapital(countryInfoBody, country);
-    });
-  };
 });
 
+// Searching by country on change event for input textbox
 countriesList.addEventListener("change", (event) => {
+  countryDetail = true;
+  console.log("Should be true", countryDetail);
   let searchedCountry = countriesList.value.toLowerCase();
   console.log("This will be the value of the input", searchedCountry);
 
@@ -62,22 +48,46 @@ countriesList.addEventListener("change", (event) => {
       console.log("ERROR", err);
       // alert("Please enter a valid country name");
     });
-
-  const displayCountryInfo = (countryData) => {
-    countries = countryData;
-
-    countries.forEach((country) => {
-      let resultsCols = createCols();
-      let countryInfo = createInfoDiv(resultsCols);
-      getFlagImg(country, countryInfo);
-      let countryInfoBody = createInfoBody(countryInfo);
-      getName(countryInfoBody, country);
-      getPopulation(countryInfoBody, country);
-      getRegion(countryInfoBody, country);
-      getCapital(countryInfoBody, country);
-    });
-  };
 });
+
+function handleCountrySelection(countryInfo) {
+  // let details = document.querySelector(".country-info");
+  details = document.querySelector(".country-info");
+
+  // details.forEach((detail) => {});
+  if (countryDetail === true) {
+    console.log("It worked!");
+    console.log(
+      "Country info in handle country selection function",
+      countryInfo
+    );
+
+    countryInfo.addEventListener("click", (event) => {
+      console.log("Country Info was clicked!!");
+      window.location.href = "detail-page/detail.html";
+    });
+  } else {
+    console.log("Nope, it didn't work");
+  }
+  // resultsCols.addEventListener("click", (event) => {
+  //   console.log("Country Info was clicked!!");
+  // });
+}
+// handleCountrySelection();
+
+function createResultsRow() {
+  let resultsRow = document.createElement("div");
+  resultsRow.setAttribute("class", "row mt-5 results-row");
+  resultsContainer.appendChild(resultsRow);
+  return resultsRow;
+}
+
+function creteResultsContainer() {
+  let resultsContainer = document.createElement("div");
+  resultsContainer.setAttribute("class", "results-container");
+  resultsSection.appendChild(resultsContainer);
+  return resultsContainer;
+}
 
 function createCols() {
   let resultsCols = document.createElement("div");
@@ -91,6 +101,7 @@ function createInfoDiv(resultsCols) {
   let countryInfo = document.createElement("div");
   countryInfo.setAttribute("class", "country-info");
   resultsCols.appendChild(countryInfo);
+
   return countryInfo;
 }
 
@@ -134,3 +145,36 @@ function getCapital(countryInfoBody, country) {
   capital.innerHTML = `Capital:
       ${country.capital}`;
 }
+
+const displayCountryInfo = (countryData) => {
+  countries = countryData;
+
+  countries.forEach((country) => {
+    // Adding to search results section
+    let resultsCols = createCols();
+    let countryInfo = createInfoDiv(resultsCols);
+    getFlagImg(country, countryInfo);
+    let countryInfoBody = createInfoBody(countryInfo);
+    getName(countryInfoBody, country);
+    getPopulation(countryInfoBody, country);
+    getRegion(countryInfoBody, country);
+    getCapital(countryInfoBody, country);
+
+    handleCountrySelection(countryInfo);
+
+    // if (countryDetail === true) {
+    //   let details = document.getElementsByClassName("country-info");
+    //   console.log("It worked!");
+
+    //   // details = document.getElementsByClassName("country-info");
+    //   details.addEventListener("click", (event) => {
+    //     console.log("Country Info was clicked!!");
+    //   });
+    // } else {
+    //   console.log("Nope, it didn't work");
+    // }
+    // // resultsCols.addEventListener("click", (event) => {
+    // //   console.log("Country Info was clicked!!");
+    // // });
+  });
+};
